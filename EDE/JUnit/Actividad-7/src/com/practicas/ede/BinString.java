e49333726ce92dc44e8f281f8bd71344c3e955a2
Ariri-21 <ariri@campusaula.com>
2017-05-19 10:47:30 +0200

    Actividad 7 EDE Completa

--- EDE/JUnit/Actividad-7/src/com/practicas/ede/BinString.java
+++ EDE/JUnit/Actividad-7/src/com/practicas/ede/BinString.java
@@ -2,16 +2,16 @@
 
 package com.practicas.ede;
 
-// ¿Qué puede hacer esta Clase?
+// ¿Qué puede hacer esta Clase? 
 public class BinString {
 	public BinString(){}
 
-	// ¿Qué hace este método?
+	// ¿Qué hace este método? Le pasamos un String este lo pasa a binario segun el codigo ascii del primer String introducido
 	public String convertir(String s){
 		return aBinario(sumar(s));
 	}
 	
-	// ¿Qué hace este método?
+	// ¿Qué hace este método?  Recibe un String y lo convierte a numeros segun su caracter en ASCII
 	public int sumar(String s){
 		String s1 = s.substring(0);
 		int sum = 0;
@@ -24,7 +24,7 @@ public class BinString {
 		return sum;
 	}
 	
-	// ¿Qué hace este método?
+	// ¿Qué hace este método? Dependiendo del numero ASCII nos dará un 1 o 0 sacando su resto con el %
 	public String aBinario(int n){
 	    String s = "";
 	    
--- EDE/JUnit/Actividad-7/src/com/practicas/ede/BinStringTest.java
+++ EDE/JUnit/Actividad-7/src/com/practicas/ede/BinStringTest.java
@@ -13,24 +13,34 @@ public class BinStringTest {
 	private BinString binString;
 	
 	public BinStringTest(){
-		// Completar
+		binString = new BinString();
 	}
 	
 	@Test
 	public void  sumarTest(){
 		// Test de la función sumar
-
+		int expected = 0;
+		assertEquals(expected, binString.sumar(""));
+		expected = 100;
+		assertEquals(expected, binString.sumar("d"));
+		expected = 292;
+		assertEquals(expected, binString.sumar("aba"));
 	}
 	
 	@Test
 	public void aBinarioTest() {
 		// Test de la función aBinario
-
+		assertEquals("1000", binString.aBinario(8));	
+		
 	}
 	
 	@Test
 	public void convertirTest() {
 		// Test de la función convertirTest
-
+		assertEquals( "111000", binString.convertir("8"));
+		assertEquals( "1000001", binString.convertir("A"));
+		
 	}
+	
+	
 }
